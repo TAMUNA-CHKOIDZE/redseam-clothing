e08a1b06347e41d742f9b7877e2a1fba4cde18eb
tamuna-tamo <102787453+TAMUNA-CHKOIDZE@users.noreply.github.com>
2025-09-20 16:57:17 +0400

    Wrap application with AuthProvider in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import "./App.css";
+import Header from "./components/Header";
+import { AuthProvider } from "./context/AuthContext";
 import AppRoutes from "./router/AppRoutes";
 
 function App() {
   return (
-    <div>
-      <AppRoutes />
+    <div
+      // style={{ border: "5px solid red" }}
+      className="max-w-[1920px] w-full mx-auto"
+    >
+      <AuthProvider>
+        <Header />
+        <AppRoutes />
+      </AuthProvider>
     </div>
   );
 }
