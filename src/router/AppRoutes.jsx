49f881dc978d3c752b28de82076caa5072c98410
tamuna-tamo <102787453+TAMUNA-CHKOIDZE@users.noreply.github.com>
2025-09-20 17:33:35 +0400

    Set up application routes with protected paths and redirection

--- src/router/AppRoutes.jsx
+++ src/router/AppRoutes.jsx
@@ -1,20 +1,50 @@
 import { Route, Routes } from "react-router-dom";
 import routes from "./routes";
-import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import ProductPage from "../pages/ProductPage";
 import Checkout from "../pages/Checkout";
 import NotFound from "../pages/NotFound";
+import ProtectedRoute from "../components/ProtectedRoute";
+import RootRedirect from "./RootRedirect";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path={routes.home} element={<Home />} />
+      {/* საიტზე შესვლისას RootRedirect-ით ვწყვეტ productList გამოუჩნდეს მომხმარებელს თუ login */}
+      <Route path="/" element={<RootRedirect />} />
       <Route path={routes.login} element={<Login />} />
       <Route path={routes.register} element={<Register />} />
-      <Route path={routes.productDetail} element={<ProductPage />} />
-      <Route path={routes.checkout} element={<Checkout />} />
+
+      {/* ProtectedRoute კომპონენტი იცავს /products, /product/:id, /checkout გვერდებს, 
+      რათა მომხმარებელმა ვერ შეძლოს გადასვლა, თუ არ არის დალოგინებული ან რეგისტრირებული. */}
+      <Route
+        path={routes.productList}
+        element={
+          <ProtectedRoute>
+            <ProductPage />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path={routes.productDetail}
+        element={
+          <ProtectedRoute>
+            <ProductPage />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path={routes.checkout}
+        element={
+          <ProtectedRoute>
+            <Checkout />
+          </ProtectedRoute>
+        }
+      />
+
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
