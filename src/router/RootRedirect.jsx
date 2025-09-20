8e899ce77bf25529439cb11e35966fa74ffe5fbf
tamuna-tamo <102787453+TAMUNA-CHKOIDZE@users.noreply.github.com>
2025-09-20 18:34:40 +0400

    Update index.css and remove Home.jsx component

--- src/router/RootRedirect.jsx
+++ src/router/RootRedirect.jsx
@@ -7,12 +7,8 @@ function RootRedirect() {
   const { isAuthenticated } = useContext(AuthContext);
   return (
     // ვიყენებ როუტის სწორად გადამისამართებისთვის: თუ ავტორიზებულია იუზერი, მაშინ საიტზე შესვლისას
-    // ავტომატურად გამოუჩინოს productList-ის გვერდი თუ არადა login-ის გვერდი. 
+    // ავტომატურად გამოუჩინოს productList-ის გვერდი თუ არადა login-ის გვერდი.
     // იქიდან შეუძლია გადავიდეს რეგისტრაციის გვერდზე თუ ჯერ არ აქვს ანგარიში.
-    // <Navigate
-    //   to={isAuthenticated ? routes.productList : routes.login}
-    //   replace
-    // />
     <Navigate to={isAuthenticated ? "/products" : "/login"} replace />
   );
 }
