880f888ad1c357ac67d4baf9dcf6caec3b06226e
tamuna-tamo <102787453+TAMUNA-CHKOIDZE@users.noreply.github.com>
2025-09-20 17:09:55 +0400

    Add RootRedirect for conditional routing: redirect authorized users to product list and others to login, with option to register.

---
+++ src/router/RootRedirect.jsx
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+import routes from "./routes";
+
+function RootRedirect() {
+  const { isAuthenticated } = useContext(AuthContext);
+  return (
+    // ვიყენებ როუტის სწორად გადამისამართებისთვის: თუ ავტორიზებულია იუზერი, მაშინ საიტზე შესვლისას
+    // ავტომატურად გამოუჩინოს productList-ის გვერდი თუ არადა login-ის გვერდი. 
+    // იქიდან შეუძლია გადავიდეს რეგისტრაციის გვერდზე თუ ჯერ არ აქვს ანგარიში.
+    <Navigate
+      to={isAuthenticated ? routes.productList : routes.login}
+      replace
+    />
+  );
+}
+
+export default RootRedirect;
