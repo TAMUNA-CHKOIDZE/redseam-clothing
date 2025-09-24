190a2c3f69e5257ad7efef880effd9d0db7471b0
tamuna-tamo <102787453+TAMUNA-CHKOIDZE@users.noreply.github.com>
2025-09-24 17:58:03 +0400

    Import CartItem component

--- src/components/CartSidebar.jsx
+++ src/components/CartSidebar.jsx
@@ -3,6 +3,7 @@ import { useCart } from "../context/CartContext";
 import { useNavigate } from "react-router-dom";
 import routes from "../router/routes";
 import empryCart from "../assets/images/oops.png";
+import CartItem from "../components/CartItem";
 
 function CartSidebar({ isOpen, onClose }) {
   const navigate = useNavigate();
@@ -31,7 +32,7 @@ function CartSidebar({ isOpen, onClose }) {
         className={`fixed top-0 right-0 w-[540px] h-full bg-white z-50 shadow-lg py-[40px] pl-[40px] transition-transform duration-300 flex flex-col justify-between ${
           isOpen ? "translate-x-0" : "translate-x-full"
         }`}
-        onClick={(e) => e.stopPropagation()} // prevents closing when clicking inside sidebar
+        onClick={(e) => e.stopPropagation()} 
       >
         <div>
           {/* Header */}
@@ -65,83 +66,20 @@ function CartSidebar({ isOpen, onClose }) {
               </button>
             </div>
           ) : (
-            <ul
+            <div
               className="flex flex-col gap-y-[36px] max-h-[300px] overflow-y-auto pr-[40px]"
               style={{ maxHeight: "calc(100vh - 490px)" }}
             >
               {cartItems.map((item, index) => (
-                <li key={index} className="flex items-start gap-x-[17px]">
-                  {/* Image */}
-                  <img
-                    src={item.image}
-                    alt={item.name}
-                    className="w-[100px] h-[134px] object-cover border border-[#E1DFE1] rounded-[10px]"
-                  />
-
-                  <div className="flex flex-col gap-y-[13px] flex-1">
-                    {/* Info */}
-                    <div className="flex justify-between gap-x-[19px]">
-                      <div className="flex flex-col gap-y-[8px]">
-                        <h3 className="font-medium text-[14px] leading-[100%] tracking-[0] text-dark-blue">
-                          {item.name}
-                        </h3>
-                        <p className="font-normal text-[12px] leading-[100%] tracking-[0] text-dark-blue-light">
-                          {item.color}
-                        </p>
-                        <p className="font-poppins font-normal text-[12px] leading-[100%] tracking-[0] text-dark-blue-light">
-                          {item.size}
-                        </p>
-                      </div>
-                      <span className="font-poppins font-medium text-[18px] leading-[100%] tracking-[0] text-dark-blue">
-                        ${item.price}
-                      </span>
-                    </div>
-
-                    {/* Quantity & Remove */}
-                    <div className="flex items-center justify-between">
-                      <div className="flex items-center justify-between w-[70px] h-[26px] rounded-[100px] px-[8px] border border-[#E1DFE1]">
-                        <button
-                          className="cursor-pointer text-[#E1DFE1]"
-                          onClick={() =>
-                            updateQuantity(
-                              item.id,
-                              item.color,
-                              item.size,
-                              item.quantity - 1
-                            )
-                          }
-                        >
-                          –
-                        </button>
-                        <span className="">{item.quantity}</span>
-                        <button
-                          className="cursor-pointer text-[#3E424A]"
-                          onClick={() =>
-                            updateQuantity(
-                              item.id,
-                              item.color,
-                              item.size,
-                              item.quantity + 1
-                            )
-                          }
-                        >
-                          +
-                        </button>
-                      </div>
-
-                      <button
-                        onClick={() =>
-                          removeFromCart(item.id, item.color, item.size)
-                        }
-                        className="font-poppins font-normal text-[12px] leading-[100%] tracking-[0] text-[#3E424A] cursor-pointer hover:text-primary transition"
-                      >
-                        Remove
-                      </button>
-                    </div>
-                  </div>
-                </li>
+                <CartItem
+                  key={index}
+                  item={item}
+                  onUpdateQuantity={updateQuantity}
+                  onRemove={removeFromCart}
+                  imageSize="small"
+                />
               ))}
-            </ul>
+            </div>
           )}
         </div>
 
